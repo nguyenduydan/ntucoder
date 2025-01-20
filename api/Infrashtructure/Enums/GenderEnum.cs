12dce7ff529e5883d668d0c327ca00ae02147303
FOG <dan.thietduynguyen@infodation.vn>
2025-01-20 14:11:15 +0700

    Add enum for gender and roles

---
+++ api/Infrashtructure/Enums/GenderEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Infrashtructure.Enums
+{
+    public enum GenderEnum
+    {
+        male = 0,
+        female = 1,
+        other = 2,
+    }
+}
---
+++ api/Infrashtructure/Enums/RoleEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Infrashtructure.Enums
+{
+    public enum RoleEnum
+    {
+        Admin = 1,
+        User = 2,
+        Manager = 3
+    }
+}
