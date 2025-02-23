9f1ba79bd9948448d1cf87994e5cfc9087629127
FOG <dn135897@gmail.com>
2025-02-23 20:31:23 +0700

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,10 +2,13 @@ import "./assets/css/App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import AuthLayout from "./layouts/auth";
 import AdminLayout from "./layouts/admin";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import initialTheme from "./theme/theme"; //  { themeGreen }
 import { useState, useEffect } from "react";
 import Loading from "./components/loading/loadingSpinner";
+import { motion, AnimatePresence } from "framer-motion";
+
+const MotionFlex = motion(Flex);
 
 export default function Main() {
   // State theme và trạng thái loading
@@ -13,11 +16,10 @@ export default function Main() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Giả lập quá trình tải (ví dụ 2 giây)
+    // Giả lập quá trình tải (ví dụ 1 giây)
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
-
+    }, 2000);
     return () => clearTimeout(timer);
   }, []);
 
@@ -26,16 +28,28 @@ export default function Main() {
       {isLoading ? (
         <Loading message="Chào mừng đến với NTUCODER" />
       ) : (
-        <Routes>
-          <Route path="auth/*" element={<AuthLayout />} />
-          <Route
-            path="admin/*"
-            element={
-              <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
-            }
-          />
-          <Route path="/" element={<Navigate to="/admin" replace />} />
-        </Routes>
+        <AnimatePresence>
+          <MotionFlex
+            key="content"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            direction="column"
+            width="100%"
+          >
+            <Routes>
+              <Route path="auth/*" element={<AuthLayout />} />
+              <Route
+                path="admin/*"
+                element={
+                  <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
+                }
+              />
+              <Route path="/" element={<Navigate to="/admin" replace />} />
+            </Routes>
+          </MotionFlex>
+        </AnimatePresence>
       )}
     </ChakraProvider>
   );
