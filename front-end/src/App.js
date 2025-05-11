6f4517a56e4197dd1f448af8cd4829ce310015f3
FOG <dn135897@gmail.com>
2025-05-12 00:21:31 +0700

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,40 +8,47 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import ProgressBar from "components/loading/ProgressBar";
 import ProtectedRoute from "components/protectedRouter/ProtectedRoute";
+import { useAuth } from "contexts/AuthContext";
+import LoadingSpinner from "components/loading/loadingSpinner";
+
 const MotionFlex = motion(Flex);
 
+
 export default function Main() {
-  // State theme và trạng thái loading
+  // State theme
   const [currentTheme, setCurrentTheme] = useState(initialTheme);
+  const { isLoading } = useAuth();
 
   return (
-    <ChakraProvider theme={currentTheme}>
-      <ProgressBar />
-      <AnimatePresence>
-        <MotionFlex
-          key="content"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 50 }}
-          transition={{ duration: 0.5, ease: "easeInOut" }}
-          direction="column"
-          width="100%"
-        >
-          <Routes>
-            <Route
-              path="admin/*"
-              element={
-                <ProtectedRoute allowedRoles={[1, 3]}>
-                  <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/*" element={
-              <UserLayout theme={currentTheme} setTheme={setCurrentTheme} />
-            } />
-          </Routes>
-        </MotionFlex>
-      </AnimatePresence>
-    </ChakraProvider>
+    isLoading ?
+      <LoadingSpinner />
+      : <ChakraProvider theme={currentTheme}>
+        <ProgressBar />
+        <AnimatePresence>
+          <MotionFlex
+            key="content"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            direction="column"
+            width="100%"
+          >
+            <Routes>
+              <Route
+                path="admin/*"
+                element={
+                  <ProtectedRoute allowedRoles={[1, 3]}>
+                    <AdminLayout theme={currentTheme} setTheme={setCurrentTheme} />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/*" element={
+                <UserLayout theme={currentTheme} setTheme={setCurrentTheme} />
+              } />
+            </Routes>
+          </MotionFlex>
+        </AnimatePresence>
+      </ChakraProvider>
   );
 }
