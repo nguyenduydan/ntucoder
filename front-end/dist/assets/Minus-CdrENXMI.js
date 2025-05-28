c8c41668409c54a1b4eaa4104db6c6e80e265773
FOG <dn135897@gmail.com>
2025-05-28 20:30:52 +0700

    update host

---
+++ front-end/dist/assets/Minus-CdrENXMI.js
@@ -0,0 +1,1 @@
+import{av as t,j as s}from"./index-Du5abbo3.js";const r=t({displayName:"MinusIcon",path:s.jsx("g",{fill:"currentColor",children:s.jsx("rect",{height:"4",width:"20",x:"2",y:"10"})})});export{r as M};
